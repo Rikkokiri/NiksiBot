bcd4e6ca5e743faa33921a365f0b8a20657b5bd8
Rikkokiri <pilvi.rajala@gmail.com>
2019-12-11 22:34:34 +0200

    Basic Firebase setup

---
+++ firebase/functions/mapping.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/* const botSmalltalkHandlers = require('./handlers/smalltalk/bots')
+const daysHandlers = require('./handlers/smalltalk/days')
+const humorHandlers = require('./handlers/smalltalk/humor')
+const weatherHandlers = require('./handlers/smalltalk/weather') */
+
+module.exports.mapHandlersToIntents = (agent) => {
+  // Run the proper function handler based on the matched Dialogflow intent name
+  let intentMap = new Map()
+
+  // Query hacks
+
+  // Smalltalk
+  /* intentMap.set('smalltalk.bots.relationshipToOtherBots', botSmalltalkHandlers.relationshipToOtherBots)
+  intentMap.set('smalltalk.whatDayIsIt', daysHandlers.whatDayIsIt)
+  intentMap.set('smalltalk.UserRequestsJoke', humorHandlers.tellAJoke)
+  intentMap.set('smalltalk.weather.userCommentsWeather', weatherHandlers.weatherComment)
+  intentMap.set('smalltalk.weather.userRequestsAgentOpinion', weatherHandlers.weatherComment) // Could be given a separate handler some day
+  intentMap.set('smalltalk.weather.userRequestsForecast', weatherHandlers.weatherForecast)
+  intentMap.set('smalltalk.weather.userRequestsForecast.anotherTime', weatherHandlers.weatherForecast) */
+
+  agent.handleRequest(intentMap)
+}
