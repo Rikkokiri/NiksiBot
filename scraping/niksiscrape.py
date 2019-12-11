f6b5e7769cfa91e423bcd66c3935808a809eaef2
Rikkokiri <pilvi.rajala@gmail.com>
2019-12-11 19:17:44 +0200

    Succesfully parsing multiple pages and storing the results in a JSON file

--- scraping/niksiscrape.py
+++ scraping/niksiscrape.py
@@ -3,29 +3,54 @@ import re
 import json
 from bs4 import BeautifulSoup
 
-url = 'https://www.pirkka.fi/niksit-ajax?page=4'
+# There are 4072 pages of niksit, so something like 40 000+ niksi
 
-response = requests.get(url)
-data = json.loads(response.text)
+url = 'https://www.pirkka.fi/niksit-ajax?page={}'
+page = 1
+niksiList = []
 
-print('TOTAL', data['total'])
+while True:
+	response = requests.get(url.format(page))
+	response_data = json.loads(response.text)
 
-soup = BeautifulSoup(data['html'], 'html.parser')
-# print(soup)
+	page += 1
 
-# For each niksi get
-# 1. Title
-# 2. Category
-# 3. Niksi text
+	if('html' not in response_data):
+		print('Empty page')
+		continue
 
-# Title
-card_titles = [c.text.strip() for c in soup.find_all('h5', {'class': 'lifehack-card-title'})]
-print(card_titles)
+	if(response_data['html'] == '\n' or page > 30):
+		print('Last page!')
+		break
 
-# Niksi text
-hacks = [h.text.strip() for h in soup.find_all('div', {'class': 'lifehack-card-text'})]
-# print(hacks)
+	# page = response_data['nextPage']
 
-# Categories
-categories = [cat.text.strip() for cat in soup.select('div.lifehack-card-category a')]
-# print(categories)
+	# For each niksi get
+	# 1. Title
+	# 2. Category
+	# 3. Niksi text
+
+	soup = BeautifulSoup(response_data['html'], 'html.parser')
+
+	# Title
+	titles = [c.text.strip() for c in soup.find_all('h5', {'class': 'lifehack-card-title'})]
+
+	# Niksi text
+	hacks = [h.text.strip() for h in soup.find_all('div', {'class': 'lifehack-card-text'})]
+
+	# Categories
+	categories = [cat.text.strip() for cat in soup.select('div.lifehack-card-category a')]
+
+	# Turn data into 'objects' that can be stored in JSON file
+	for (title, cat, hack) in list(zip(titles, categories, hacks)):
+		niksi = {'title': title, 'category': cat, 'text': hack}
+		niksiList.append(niksi)
+
+	parseCount = len(niksiList)
+	percentage = round(parseCount / (4072 * 11), 4)
+	print('Page {}: {} niksis parsed, (approximately {} %% of all data)'.format(page-1, parseCount, percentage))
+
+# Write the retrieved data to a JSON file
+with open('niksidata.json', 'w') as outfile:
+	json.dump(niksiList, outfile, ensure_ascii=False)
+
